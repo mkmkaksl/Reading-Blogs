f9c3dfc878fba21ead5f12dcd67fb76e53f8d9f3
Manoj <manojjeeramani@gmail.com>
2022-08-12 10:04:41 -0500

    script CDN added and error checking

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    const jQueryScript = document.getElementById("jQueryScript")
+    jQueryScript.setAttribute("src", "https://code.jquery.com/jquery-3.6.0.slim.min.js")
+    jQueryScript.onerror = function() {
+        jQueryScript.setAttribute("src", "node_modules/jquery/dist/jquery.slim.min.js")
+    }
+    const bootstrapScript = document.getElementById("bootstrapScript")
+    bootstrapScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js")
+    bootstrapScript.onerror = function() {
+        bootstrapScript.setAttribute("src", "node_modules/bootstrap/dist/js/bootstrap.min.js")
+    }
+    const bootstrapCSS = document.getElementById("bootstrapCSS")
+    bootstrapCSS.setAttribute("src", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css")
+    bootstrapCSS.onerror = function() {
+        bootstrapCSS.setAttribute("src", "node_modules/bootstrap/dist/css/bootstrap.min.css")
+    }
+})
